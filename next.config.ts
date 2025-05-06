909075625a4777d60412f0d14b0aac24d51cf61d
shubhanshu-02 <shubhanshu004@gmail.com>
2025-05-06 18:23:34 +0530

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Keep React Strict Mode enabled
+  trailingSlash: true, // Replaces exportTrailingSlash with the correct option
 };
 
 export default nextConfig;
